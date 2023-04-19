d15ef39b83e30120e1186e6ba315d9d8c086e8d5
maclandrol <emmanuel.noutahi@hotmail.ca>
2023-04-18 22:12:33 -0400

    remove unecessary comment

--- molfeat/trans/base.py
+++ molfeat/trans/base.py
@@ -634,8 +634,6 @@ class MoleculeTransformer(TransformerMixin, BaseFeaturizer, metaclass=_Transform
                     klass_name
                 ].from_state_dict(args["bond_featurizer"])
             else:
-                # buffer = io.BytesIO(bytes.fromhex(args["bond_featurizer"]))
-                # args["bond_featurizer"] = joblib.load(buffer)
                 args["bond_featurizer"] = hex_to_fn(args["bond_featurizer"])
             args.pop("_bond_featurizer_is_pickled", None)
         ## Deal with custom featurizer
