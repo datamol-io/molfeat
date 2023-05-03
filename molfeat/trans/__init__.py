017139cc911089beb3aa271e9fd348b371242d8b
maclandrol <emmanuel.noutahi@hotmail.ca>
2023-05-03 00:34:01 -0400

    add shortcut for importing FPVec

--- molfeat/trans/__init__.py
+++ molfeat/trans/__init__.py
@@ -1,3 +1,4 @@
 from molfeat.trans.concat import FeatConcat
 from molfeat.trans.base import BaseFeaturizer
 from molfeat.trans.base import MoleculeTransformer
+from molfeat.trans.fp import FPVecTransformer
