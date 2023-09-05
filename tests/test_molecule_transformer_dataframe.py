f9a8b937e66fdb1b201c4eae798f4894dcac97e7
Emmanuel Noutahi <emmanuel.noutahi@hotmail.ca>
2023-09-05 15:27:53 +0200

    wip ruff

--- tests/test_molecule_transformer_dataframe.py
+++ tests/test_molecule_transformer_dataframe.py
@@ -11,50 +11,46 @@ from molfeat.trans.base import MoleculeTransformer
 
 @pytest.fixture
 def smiles():
-    return ['CC1CC2C3CCC4=CC(=O)C=CC4(C3(C(CC2(C(=O)CO1)O)C)O)C',
-            'CN(CCOC(c1ccccc1)c1ccccc1)C',
-            'O/N=C(/c1csc(n1)N)\C(=O)N[C@@H]1C(=O)N2[C@@H]1SCC(=C2C(=O)O)C=C',
-            'CC(C)(C)NCC(C1=CC(=C(C=C1)O)CO)O']
+    return [
+        "CC1CC2C3CCC4=CC(=O)C=CC4(C3(C(CC2(C(=O)CO1)O)C)O)C",
+        "CN(CCOC(c1ccccc1)c1ccccc1)C",
+        "O/N=C(/c1csc(n1)N)\C(=O)N[C@@H]1C(=O)N2[C@@H]1SCC(=C2C(=O)O)C=C",
+        "CC(C)(C)NCC(C1=CC(=C(C=C1)O)CO)O",
+    ]
 
 
-@pytest.fixture(params=[
-    'list',
-    'series',
-    'dataframe'
-])
+@pytest.fixture(params=["list", "series", "dataframe"])
 def mols(request, smiles):
-    if request.param == 'list':
+    if request.param == "list":
         return smiles
-    elif request.param == 'series':
-        return pd.Series(smiles, name='smiles')
-    elif request.param == 'dataframe':
-        return pd.DataFrame({'smiles': smiles, 'column_2': [1, 0, 1, 1]})
+    elif request.param == "series":
+        return pd.Series(smiles, name="smiles")
+    elif request.param == "dataframe":
+        return pd.DataFrame({"smiles": smiles, "column_2": [1, 0, 1, 1]})
 
 
 def test_list_series_dataframe(mols):
-    transformer_ecfp = MoleculeTransformer(featurizer='ecfp')
+    transformer_ecfp = MoleculeTransformer(featurizer="ecfp")
     results = transformer_ecfp.fit_transform(mols)
 
     assert results.shape == (4, 2048)
     assert isinstance(results, np.ndarray)
 
+
 def test_with_pipeline_column_transformer(smiles):
     # setup data
-    mols = pd.DataFrame({'smiles': smiles, 'column_2': [1, 0, 1, 1]})
+    mols = pd.DataFrame({"smiles": smiles, "column_2": [1, 0, 1, 1]})
 
     # setup pipeline
-    transformer_ecfp = MoleculeTransformer(featurizer='ecfp')
+    transformer_ecfp = MoleculeTransformer(featurizer="ecfp")
     column_preprocessor = ColumnTransformer(
         transformers=[
-            ('ecfp_trans', transformer_ecfp, ['smiles']),
-            ('column_2', 'passthrough', ['column_2'])
+            ("ecfp_trans", transformer_ecfp, ["smiles"]),
+            ("column_2", "passthrough", ["column_2"]),
         ]
     )
 
-    pipeline = Pipeline([
-        ('preprocess', column_preprocessor),
-        ('classifier', GaussianNB())
-    ])
+    pipeline = Pipeline([("preprocess", column_preprocessor), ("classifier", GaussianNB())])
 
     # fit/predict pipeline
     pipeline.fit(mols, [1, 0, 1, 0])
@@ -65,4 +61,4 @@ def test_with_pipeline_column_transformer(smiles):
     assert isinstance(r, expect)
 
     expect = (4,)
-    assert r.shape==expect
+    assert r.shape == expect
